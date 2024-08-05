e15c0d8b7c3b121b5ca359c3c1569958f9a0f1b9
vknanavati <vimin.nanavati@gmail.com>
2024-08-05 18:16:44 -0400

    first commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {Link, Routes, Route} from 'react-router-dom';
+import {Home} from './components/Home';
+import {ToDoList} from './components/ToDoList';
+import {Weather} from './components/Weather';
+import {Currency} from './components/Currency';
+import {Countdown} from './components/Countdown';
+import {Counter} from './components/Counter';
+import {Contact} from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/todo">To Do List</Link>
+          </li>
+          <li>
+            <Link to="/weather">Weather</Link>
+          </li>
+          <li>
+            <Link to="/currency">Currency Converter</Link>
+          </li>
+          <li>
+            <Link to="/countdown">Countdown</Link>
+          </li>
+          <li>
+            <Link to="/counter">Counter</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Me</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/todo" element={<ToDoList/>}/>
+        <Route path="/weather" element={<Weather/>}/>
+        <Route path="/currency" element={<Currency/>}/>
+        <Route path="/countdown" element={<Countdown/>}/>
+        <Route path="/counter" element={<Counter/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </>
   );
 }
 
