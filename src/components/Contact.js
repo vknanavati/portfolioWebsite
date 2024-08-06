5a632333034078d8c400455f3ec678a34693e14b
vknanavati <vimin.nanavati@gmail.com>
2024-08-06 15:41:55 -0400

    added contact page.

---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+
+export function Contact() {
+    return (
+    <>
+        <h1>Get in touch with me!</h1>
+        <div>
+            <div>
+                <img alt="email-logo" src="/src/icons/email-logo.png"/>
+                <a href="mailto:vimin.nanavati@gmail.com">vimin.nanavati@gmail.com</a>
+            </div>
+            <div>
+                <a href="https://github.com/vknanavati" target="_blank" rel="noopener noreferrer">Github</a>
+            </div>
+            <div>
+            <a href="https://www.linkedin.com/in/vimin-nanavati-3510555b/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            </div>
+        </div>
+    </>
+)
+}
