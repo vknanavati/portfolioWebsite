0ba85bce5f9ad5b6b2d2deb782c1e704a3e5805f
vknanavati <vimin.nanavati@gmail.com>
2024-11-06 10:04:21 -0500

    shifted code to the left.

--- src/components/recipe/RecipeCard.js
+++ src/components/recipe/RecipeCard.js
@@ -6,50 +6,50 @@ import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 
 export function RecipeCard ({recipe, addFavorite, addMakeRecipe, favorites}) {
 
-    const isFavorite = favorites && favorites.some(item => item.label === recipe.label);
+  const isFavorite = favorites && favorites.some(item => item.label === recipe.label);
 
-    // console.log("recipe", recipe)
-    return (
-        <Grid
-            sm={8}
-            sx={{
-              boxShadow: 6,
-              margin: 4,
-              padding: 2,
-              textAlign: "center",
-              width: "370px",
-              height: "auto",
-              borderRadius: 3
-            }}
-        >
-          {isFavorite ? (
-              <IconButton
-              variant="plain"
-              onClick={()=>addFavorite(recipe)}
-            >
-              <FavoriteOutlined sx={{ color: "red"}}/>
-            </IconButton>
-          ):(
-            <IconButton onClick={()=>addFavorite(recipe)}>
-              <FavoriteBorder/>
-           </IconButton>
-          )}
+  // console.log("recipe", recipe)
+  return (
+      <Grid
+          sm={8}
+          sx={{
+            boxShadow: 6,
+            margin: 4,
+            padding: 2,
+            textAlign: "center",
+            width: "370px",
+            height: "auto",
+            borderRadius: 3
+          }}
+      >
+        {isFavorite ? (
+            <IconButton
+            variant="plain"
+            onClick={()=>addFavorite(recipe)}
+          >
+            <FavoriteOutlined sx={{ color: "red"}}/>
+          </IconButton>
+        ):(
+          <IconButton onClick={()=>addFavorite(recipe)}>
+            <FavoriteBorder/>
+          </IconButton>
+        )}
 
-          <Grid>
-            <Button
-              onClick={()=>addMakeRecipe(recipe)}
-            >
-              Make Recipe
-            </Button>
-          </Grid>
-          <Typography variant="h6" >{recipe.label}</Typography>
-          <img alt="food-photo" src={recipe.image}/>
+        <Grid>
+          <Button
+            onClick={()=>addMakeRecipe(recipe)}
+          >
+            Make Recipe
+          </Button>
+        </Grid>
+        <Typography variant="h6" >{recipe.label}</Typography>
+        <img alt="food-photo" src={recipe.image}/>
 
-          <Typography sx={{wordWrap: "break-word", marginTop: 2}}>
-              <a href={recipe.url} target="_blank" rel="noopener noreferrer">
-            Full Recipe
-              </a>
-          </Typography>
-      </Grid>
+        <Typography sx={{wordWrap: "break-word", marginTop: 2}}>
+            <a href={recipe.url} target="_blank" rel="noopener noreferrer">
+          Full Recipe
+            </a>
+        </Typography>
+    </Grid>
   )
 }
