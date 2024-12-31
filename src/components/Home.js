011cf31cf049bc5362412f193d3c16d7de4af158
vknanavati <vimin.nanavati@gmail.com>
2024-12-31 16:54:20 -0500

    added MUI icons for linkedin, github, and email in place of components holding png icons, formatted background.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import Grid from '@mui/material/Grid2';
 import {Container, Box} from '@mui/material';
 import BACKGROUND from "../images/background.png"
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 export function Home() {
     return (
@@ -9,131 +12,87 @@ export function Home() {
                 <Box
                     sx={{
                         backgroundImage: `url(${BACKGROUND})`,
-                        paddingBottom: 20
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        width: '100vw',
+                        height: '100vh',
+                        display: 'flex',
+                        flexDirection: 'column'
+
                     }}
                 >
                     <Grid
                         container
-                        justifyContent={"space-evenly"}
                         alignItems={"center"}
+                        justifyContent={"center"}
+                        sx={{ flex: 1 }}
                     >
-                        <Grid
-                            sx={{
-                                flexWrap: "nowrap",
-                                marginTop: 20,
-                                marginBottom: 10
-                             }}>
-                            <Box
-                                component="a"
-                                href="https://vknanavati.github.io/portfolioWebsite/#/recipe/homeRecipe"
-                            >
-                                <Grid
-                                    component="img"
-                                    alt="recipe-search-image"
-                                    src={`${process.env.PUBLIC_URL}/icons/recipeSearch.png`}
-                                    sx={{
-                                        maxWidth: 750,
-                                        transition: "transform 0.3s ease-in-out",
-                                        "&:hover": {
-                                            transform: "scale(1.10)",
-                                            boxShadow: 5,
-                                            borderRadius: 3
-                                        },
-                                    }}
-                                />
-                            </Box>
-                        </Grid>
-                        <Grid
-                            sx={{
-                                flexWrap: "nowrap",
-                                marginTop: 5,
-                                marginBottom: 10
-                             }}
-                        >
-                            <Box
-                                component="a"
-                                href="https://vknanavati.github.io/portfolioWebsite/#/todo"
-                            >
-                                <Grid
-                                    component="img"
-                                    alt="to-do-list-image"
-                                    src={`${process.env.PUBLIC_URL}/icons/todolist.png`}
-                                    sx={{
-                                        maxWidth: 700,
-                                        marginRight: 12,
-                                        transition: "transform 0.3s ease-in-out",
-                                        "&:hover": {
-                                            transform: "scale(1.10)",
-                                            boxShadow: 5,
-                                            borderRadius: 3
-                                        },
-                                    }}
-                                />
-                            </Box>
-                            <Box
-                                component="a"
-                                href="https://vknanavati.github.io/portfolioWebsite/#/weather"
-                            >
-                                <Grid
-                                    component="img"
-                                    alt="weather-image"
-                                    src={`${process.env.PUBLIC_URL}/icons/weather.png`}
-                                    sx={{
-                                        maxWidth: 300,
-                                        transition: "transform 0.3s ease-in-out",
-                                        "&:hover": {
-                                            transform: "scale(1.10)",
-                                            boxShadow: 5,
-                                            borderRadius: 3
-                                        },
-                                    }}
-                                />
-                            </Box>
-                        </Grid>
-                    <Grid>
-                        <Box
-                            component="a"
-                            href="https://vknanavati.github.io/portfolioWebsite/#/countdown"
-                        >
-                            <Grid
-                                component="img"
-                                alt="countdown-image"
-                                src={`${process.env.PUBLIC_URL}/icons/countdown.png`}
-                                sx={{
-                                    maxWidth: 700,
-                                    marginRight: 10,
-                                    transition: "transform 0.3s ease-in-out",
-                                        "&:hover": {
-                                            transform: "scale(1.10)",
-                                            boxShadow: 5,
-                                            borderRadius: 3
-                                        },
-                                }}
-                            />
-                        </Box>
-                        <Box
-                            component="a"
-                            href="https://vknanavati.github.io/portfolioWebsite/#/counter"
-                        >
-                            <Grid
-                                component="img"
-                                alt="counter-image"
-                                src={`${process.env.PUBLIC_URL}/icons/counter.png`}
-                                sx={{
-                                    maxWidth: 500,
-                                    transition: "transform 0.3s ease-in-out",
-                                        "&:hover": {
-                                            transform: "scale(1.10)",
-                                            boxShadow: 5,
-                                            borderRadius: 3
-                                        },
-                                }}
-                            />
-                        </Box>
                     </Grid>
-                </Grid>
-            </Box>
-        </Grid>
+
+                    <Grid
+                        container
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ marginBottom: '20px', gap: 4 }}
+
+                    >
+
+                        <Grid
+                            component="a"
+                            href="mailto:vimin.nanavati@gmail.com"
+                        >
+                            <EmailIcon
+                                sx={{
+                                    fontSize: 70,
+                                    color: '#00FFFF',
+                                    filter: 'drop-shadow(0 0 10px #00FFFF) drop-shadow(0 0 20px #00FFFF)',
+                                    '&:hover': {
+                                        filter: 'drop-shadow(0 0 20px #00FFFF) drop-shadow(0 0 30px #00FFFF)',
+                                    }
+                                }}
+                            />
+                        </Grid>
+                        <Grid
+                            component="a"
+                            href="https://github.com/vknanavati"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <GitHubIcon
+                                sx={{
+                                    fontSize: 70,
+                                    color: '#00FFFF',
+                                    filter: 'drop-shadow(0 0 10px #00FFFF) drop-shadow(0 0 20px #00FFFF)',
+                                    '&:hover': {
+                                        filter: 'drop-shadow(0 0 20px #00FFFF) drop-shadow(0 0 30px #00FFFF)',
+                                    }
+                                }}
+                            />
+                        </Grid>
+
+                        <Grid
+                            component="a"
+                            href="https://www.linkedin.com/in/vimin-nanavati-3510555b/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <LinkedInIcon
+                                sx={{
+                                    fontSize: 70,
+                                    color: '#00FFFF',
+                                    filter: 'drop-shadow(0 0 10px #00FFFF) drop-shadow(0 0 20px #00FFFF)',
+                                    '&:hover': {
+                                        filter: 'drop-shadow(0 0 20px #00FFFF) drop-shadow(0 0 30px #00FFFF)',
+                                    }
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+
+                </Box>
+            </Grid>
+
     </Container>
 )
 }
