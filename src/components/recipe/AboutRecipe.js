04f94543f590b8b06b8106e8d0ecee422f05b44f
vknanavati <vimin.nanavati@gmail.com>
2025-01-05 19:17:34 -0500

    formatted elements to match theme.

--- src/components/recipe/AboutRecipe.js
+++ src/components/recipe/AboutRecipe.js
@@ -8,14 +8,27 @@ export function AboutRecipe() {
         <Grid container justifyContent={"center"} direction={"column"} alignItems={"center"}>
 
             <Grid item sx={{marginTop: 4, marginBottom: 3}}>
-                <Typography variant="h4">How to Use </Typography>
+                <Typography
+                variant="h4"
+                sx={{
+                    textShadow: '0 0 20px #00FFFF, 0 0 20px #00FFFF, 0 0 10px #00FFFF',
+                    fontFamily: "'Tilt Neon', sans-serif",
+                    fontSize: 40,
+                    color: '#AEFFFF',
+                    fontWeight: 800,
+                }}
+                >
+                    How to Use
+                </Typography>
             </Grid>
 
-            <Grid item sx={{ textAlign: "left" }}>
-                <Typography variant="body1" fontSize={21}>After clicking on the cusine type of your choice, recipe cards will appear.</Typography>
+            <Grid item sx={{ textAlign: "left", backgroundColor: "white", padding: 2,}}>
+                <Typography variant="body1" fontSize={21}
+                >
+                    After clicking on the cusine type of your choice, recipe cards will appear.</Typography>
             </Grid>
 
-            <Grid item sx={{ textAlign: "left" }}>
+            <Grid item sx={{ textAlign: "left", backgroundColor: "white", marginBottom: 2, padding: 2}}>
                 <Typography variant="body1" fontSize={21}
                 >
                     Each recipe card contains:
@@ -40,10 +53,10 @@ export function AboutRecipe() {
                 sx={{maxWidth: 370}}
             />
 
-            <Grid item sx={{marginTop: 4, marginBottom: 3}}>
+            <Grid item sx={{marginTop: 4, padding: 2, backgroundColor: "white"}}>
                 <Typography variant="h5">Make Recipe</Typography>
             </Grid>
-            <Grid item sx={{ textAlign: "left", marginBottom: 2 }}>
+            <Grid item sx={{ textAlign: "left", marginBottom: 2, padding: 2, backgroundColor: "white" }}>
                 <Typography variant="body1" fontSize={21}
                 >
                     Clicking 'Make Recipe' will store the card in the 'Recipes' page. Here you can create a grocery list
