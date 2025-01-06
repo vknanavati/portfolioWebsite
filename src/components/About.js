349253707f6035143666c98dd7e827a1e3703c81
vknanavati <vimin.nanavati@gmail.com>
2025-01-06 18:05:40 -0500

    formatted to match theme.

--- src/components/About.js
+++ src/components/About.js
@@ -7,28 +7,33 @@ export function About() {
         <Container sx={{paddingBottom: 20}}>
             <Grid>
                 <Typography
-                    variant="h4"
-                    marginTop={15}
+                    marginTop={18}
                     marginBottom={3}
                     textAlign={"center"}
-                    color={"#666666"}
-                    fontWeight={700}
-                    fontSize={60}
+                    sx={{
+                        textShadow: '0 0 20px #00FFFF, 0 0 20px #00FFFF, 0 0 10px #00FFFF',
+                        fontFamily: "'Tilt Neon', sans-serif",
+                        fontSize: 70,
+                        color: '#AEFFFF',
+                        fontWeight: 800
+                    }}
                 >
                     About Me
                 </Typography>
                 <Grid
-                  sx={{
-                    backgroundColor: "#e6d7ff",
-                    padding: 4,
-                    borderRadius: 7,
-                    boxShadow: 6
-                }}
+                    sx={{
+                        background: "linear-gradient(145deg, #140A26F2, #580F58E6)",
+                        padding: 4,
+                        borderRadius: 7,
+                        boxShadow: 6
+                    }}
                 >
                     <Grid>
                         <Typography
                             variant="body"
-                            fontSize={25}
+                            fontSize={30}
+                            color="#80DEEA"
+                            sx={{fontFamily: "'Tilt Neon', sans-serif"}}
                         >
                             My name is Vimin Nanavati and I am a self-taught developer
                             with experience in HTML, CSS, JavaScript, jQuery, React, React Router and Material UI.
@@ -37,60 +42,53 @@ export function About() {
                         </Typography>
                     </Grid>
                 </Grid>
-
-                <Typography
-                    variant="h5"
+                <Grid
                     marginBottom={2}
                     marginTop={3}
-                    color={"#666666"}
-                    fontWeight={555}
-                    fontSize={40}
-                >
-                    Projects
-                </Typography>
-                <Grid
                     sx={{
-                        backgroundColor: "#e6d7ff",
+                        background: "linear-gradient(145deg, #140A26F2, #580F58E6)",
+                        fontFamily: "'Tilt Neon', sans-serif",
+                        color: "#80DEEA",
                         padding: 3,
                         borderRadius: 7,
-                        fontSize: 25,
+                        fontSize: 30,
                         boxShadow: 6,
                     }}
                 >
                     <Grid marginBottom={1}>
                         <Typography variant="body">
-                        My first project was building a simple counter app.
-                        Through this app I learned the basics of React and the useState hook.
+                            My first project was building a simple counter app.
+                            Through this app I learned the basics of React and the useState hook.
                         </Typography>
                     </Grid>
                     <Grid marginBottom={1}>
                         <Typography variant="body">
-                        I expanded my understanding of useState and began exploring
-                        error handling through a To Do List app which allows the user
-                        to add a task and delete as needed.
+                            I expanded my understanding of useState and began exploring
+                            error handling through a To Do List app which allows the user
+                            to add a task and delete as needed.
                         </Typography>
                     </Grid>
                     <Grid marginBottom={1}>
                         <Typography variant="body">
-                        Building a Countdown app introduced me to useEffect, where I
-                        learned regex for error handling.
+                            Building a Countdown app introduced me to useEffect, where I
+                            learned regex for error handling.
                         </Typography>
                     </Grid>
                     <Grid marginBottom={1}>
                         <Typography variant="body">
-                        I learned to work with APIs and useEffect through
-                        my Currency Converter app.
+                            I learned to work with APIs and useEffect through
+                            my Currency Converter app.
                         </Typography>
                     </Grid>
                     <Grid marginBottom={1}>
                         <Typography variant="body">
-                        I further practiced API integration in React by building a weather application
-                        that displays live weather data.
+                            I further practiced API integration in React by building a weather application
+                            that displays live weather data.
                         </Typography>
                     </Grid>
                     <Grid>
                         <Typography variant="body">
-                        Each project was styled using MaterialUI.
+                            Each project was styled using MaterialUI.
                         </Typography>
                     </Grid>
                 </Grid>
