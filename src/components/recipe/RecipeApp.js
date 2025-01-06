ad23027e8a3affb52de7a7093a14ba39904c152e
vknanavati <vimin.nanavati@gmail.com>
2025-01-05 22:57:42 -0500

    formatted to match neon theme.

--- src/components/recipe/RecipeApp.js
+++ src/components/recipe/RecipeApp.js
@@ -4,9 +4,29 @@ import {Link, Outlet} from 'react-router-dom';
 
 export function RecipeApp({makeRecipe, favorites}) {
 
+  const navLinkStyles = {
+    color: "#00F7FF",
+    textShadow: "0 0 8px #00F7FF",
+    '&:hover': {
+      color: "#40FBFF",
+      textShadow: `
+        0 0 8px #00F7FF,
+        0 0 12px #00F7FF,
+        0 0 16px #00F7FF,
+        0 0 24px #00F7FF
+      `
+    }
+  };
+
   return (
     <Container maxWidth={"false"} disableGutters>
-      <AppBar position="fixed" sx={{backgroundColor: "#F61297", marginTop: "128px"}}>
+      <AppBar
+        position="fixed"
+        sx={{
+          marginTop: "128px",
+          background: "linear-gradient(to right, #0D0D19E6, #260D26E6)",
+        }}
+      >
         <Toolbar>
           <Box sx={{
             display: "flex",
@@ -20,16 +40,16 @@ export function RecipeApp({makeRecipe, favorites}) {
             overflow: "auto",
             flexShrink: 1
           }}>
-            <Typography variant="h6" component={Link} to="/recipe/homeRecipe" color="inherit" sx={{ textDecoration: 'none' }}>
+            <Typography variant="h6" component={Link} to="/recipe/homeRecipe"  sx={{ textDecoration: 'none', ...navLinkStyles }}>
               Home
             </Typography>
-            <Typography variant="h6" component={Link} to="/recipe/aboutRecipe" color="inherit" sx={{ textDecoration: 'none' }}>
+            <Typography variant="h6" component={Link} to="/recipe/aboutRecipe" color="inherit" sx={{ textDecoration: 'none', ...navLinkStyles }}>
               About
             </Typography>
-            <Typography variant="h6" component={Link} to="/recipe/favorites" color="inherit" sx={{ textDecoration: 'none' }}>
+            <Typography variant="h6" component={Link} to="/recipe/favorites" color="inherit" sx={{ textDecoration: 'none', ...navLinkStyles }}>
               Favorites ({favorites !== null && (favorites.length)})
             </Typography>
-            <Typography variant="h6" component={Link} to="/recipe/make" color="inherit" sx={{ textDecoration: 'none' }}>
+            <Typography variant="h6" component={Link} to="/recipe/make" color="inherit" sx={{ textDecoration: 'none', ...navLinkStyles }}>
               Recipes ({makeRecipe.length !== null && (makeRecipe.length) })
               {console.log("****MAKE RECIPE NAV LINK: ", makeRecipe.length > 0 && (makeRecipe.length))}
             </Typography>
