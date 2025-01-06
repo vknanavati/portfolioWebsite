b23013e32000cf1ce54372a53cfa6825da2cc685
vknanavati <vimin.nanavati@gmail.com>
2025-01-05 20:10:09 -0500

    added backgroundAttachment to prevent background image from resizing when recipe cards populate.

--- src/components/Wallpaper.js
+++ src/components/Wallpaper.js
@@ -13,8 +13,8 @@ export function Wallpaper({children}) {
             minHeight: '100vh',
             display: 'flex',
             flexDirection: 'column',
-            overflow: 'hidden'
-
+            overflow: 'hidden',
+            backgroundAttachment: 'fixed'
         }}
         >
             <Box
