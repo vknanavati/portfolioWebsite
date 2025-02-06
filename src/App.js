88cdf1d958bb950dc493dff5cfedb1da5f3265d3
vknanavati <vimin.nanavati@gmail.com>
2025-02-06 11:44:38 -0500

    added TS components from TS repository, fixed addMakeRecipe function to clear components from MakeRecipe page when recipe removed.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react';
 import {Link, Routes, Route} from 'react-router-dom';
 import {Home} from './components/Home';
 import {About} from './components/About';
-import {RecipeApp} from './components/recipe/RecipeApp';
-import {Favorites} from './components/recipe/Favorites';
-import {MakeRecipe} from './components/recipe/MakeRecipe';
-import {AboutRecipe} from './components/recipe/AboutRecipe';
-import {HomeRecipe} from './components/recipe/HomeRecipe';
+import {RecipeAppTS} from './components/recipe/RecipeAppTS';
+import {FavoritesTS} from './components/recipe/FavoritesTS';
+import {MakeRecipeTS} from './components/recipe/MakeRecipeTS';
+import {AboutRecipeTS} from './components/recipe/AboutRecipeTS';
+import {HomeRecipeTS} from './components/recipe/HomeRecipeTS';
 import {Hostel} from './components/Hostel';
 import {ToDoList} from './components/ToDoList';
 import {Weather} from './components/Weather';
@@ -139,20 +139,33 @@ function App() {
   };
 
   const addMakeRecipe = (recipe) => {
+    // if condition true, recipe not in makeRecipe, recipe is added to makeRecipe
     if (!(makeRecipe.filter(item => item.label === recipe.label).length > 0)){
       setMakeRecipe([...makeRecipe, recipe]);
+
       setAlertRecipe(true);
+
       setTimeout(()=>{
-        setAlertRecipe(false)
+        setAlertRecipe(false);
       }, 3000)
+
       console.log("makeRecipe: ", makeRecipe);
       console.log("recipe added to makeRecipe: ", recipe);
+
     } else {
-        setMakeRecipe(makeRecipe.filter((item)=> item.label !== recipe.label));
+        // check item in makeRecipe and see if item.label in array does not match recipe.label
+        //not matching the label is the condition so it will create a new array of recipes that don't match the recipe.label
+        const updatedMakeRecipe = makeRecipe.filter((item)=> item.label !== recipe.label);
+        setMakeRecipe(updatedMakeRecipe);
+
+        setFilteredRecipe([]);
+
         setAlertRemove(true);
-        setTimeout(()=>{
-          setAlertRemove(false)
+
+        setTimeout(()=> {
+          setAlertRemove(false);
         }, 3000);
+
         console.log("recipe removed: ", recipe);
     }
   };
@@ -384,12 +397,12 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
 
-        <Route path="/recipe" element={<RecipeApp favorites={favorites} makeRecipe={makeRecipe}/>}>
-          <Route path="aboutRecipe" element={<AboutRecipe/>}/>
+        <Route path="/recipe" element={<RecipeAppTS favorites={favorites} makeRecipe={makeRecipe}/>}>
+          <Route path="aboutRecipe" element={<AboutRecipeTS/>}/>
           <Route
             path="homeRecipe"
             element={
-              <HomeRecipe
+              <HomeRecipeTS
                 addFavorite={addFavorite}
                 foodData={foodData}
                 setFoodData={setFoodData}
@@ -401,14 +414,14 @@ function App() {
             }
           />
           <Route path="favorites" element={
-            <Favorites
+            <FavoritesTS
               favorites={favorites}
               addFavorite={addFavorite}
               addMakeRecipe={addMakeRecipe}/>
               }
             />
           <Route path="make" element={
-            <MakeRecipe
+            <MakeRecipeTS
               makeRecipe={makeRecipe}
               addGrocery={addGrocery}
               groceryList={groceryList}
