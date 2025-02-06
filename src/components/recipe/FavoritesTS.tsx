a9fa9a2730f2781d64ce3ee04dcb6dc90885526c
vknanavati <vimin.nanavati@gmail.com>
2025-02-06 11:22:18 -0500

    added tsx component from ts repository.

---
+++ src/components/recipe/FavoritesTS.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { RecipeCardTS } from './RecipeCardTS'
+import { Recipe } from '../../../types';
+import Grid from '@mui/material/Grid2';
+import { Container, Typography, useTheme, useMediaQuery } from '@mui/material';
+
+interface FavoritesProps {
+    favorites: Recipe[]
+    addFavorite: (recipe: Recipe) => void
+    addMakeRecipe: (recipe: Recipe) => void
+}
+export function FavoritesTS ({favorites, addFavorite, addMakeRecipe}: FavoritesProps) {
+
+    console.log("FAVORITES TSX RENDERING")
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+    return (
+        <Container sx={{ paddingTop: "64px" }}>
+            <Grid
+                container
+                justifyContent={"center"}
+                direction={"column"}
+                alignItems={"center"}
+            >
+                <Grid>
+                    <Typography
+                        variant="h4"
+                        sx={{ marginTop: 11, marginBottom:2 }}
+                    >
+                        Favorite Recipes
+                    </Typography>
+                </Grid>
+                {favorites.length > 0 && (
+                    <Grid
+                        container
+                        justifyContent={ "center" }
+                        sx={{
+                            maxWidth: "100%",
+                            flexWrap: "wrap",
+                        }}
+                    >
+                        {favorites.map((recipe,index) => (
+                            <Grid
+                                sx={{
+                                    width: isMobile ? { xs: "100%", sm: "50%", md: "33.33%" } : "100%",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                }}
+                            >
+                            <RecipeCardTS
+                                key={index}
+                                recipe={recipe}
+                                addFavorite={addFavorite}
+                                favorites={favorites}
+                                addMakeRecipe={addMakeRecipe}
+                            />
+                            </Grid>
+                        ))}
+                    </Grid>
+                )}
+            </Grid>
+        </Container>
+    )
+}
